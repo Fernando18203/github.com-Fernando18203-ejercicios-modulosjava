124bf262d76f77151cc4bc998ca4afe8187d18d0
Fernando18203 <25680119@cuautla.tecnm.mx>
2025-12-05 19:19:45 -0600

    Add MainProductoDescuento class with main method

---
+++ src/ejercicio8_descuento/MainProductoDescuento.java
@@ -0,0 +1,7 @@
+public class MainProductoDescuento {
+
+    public static void main(String[] args) {
+        ControlProductoDescuento control = new ControlProductoDescuento();
+        control.ejecutar();
+    }
+}
